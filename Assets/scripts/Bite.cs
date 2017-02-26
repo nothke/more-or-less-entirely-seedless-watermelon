c51846be973ffcc8e598d3e33502f001c98e92b6
Pepijn Willekens <pepijnwillekens@gmail.com>
2017-02-26 16:18:06 +0100

    started bit mechanic

---
+++ Assets/scripts/Bite.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Bite : MonoBehaviour {
+    public static List<Bite> bites;
+    public float closenessRadius;
+    public AudioClip sound;
+    private void Awake()
+    {
+        bool valid = false;
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, closenessRadius);
+        for (int i = 0; i < cols.Length; i++)
+        {
+            if (valid) break;
+            valid |= cols[i].GetComponent<Bite>();
+        }
+        cols = Physics2D.OverlapPointAll(transform.position);
+        for (int i = 0; i < cols.Length; i++)
+        {
+            if (!valid) break;
+            valid &= cols[i].GetComponent<Bite>();
+        }
+        valid &= cols.Any((e) => { return e.GetComponent<ExtraDrag>() ; });
+
+        if (valid)
+        {
+            bites.Add(this);
+            if (sound) {
+                AudioSource.PlayClipAtPoint(sound, Camera.main.transform.position);
+            }
+        }else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/Eater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Eater : MonoBehaviour {
+
+    public Bite prefab;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        Touch[] touches = Input.touches;
+        touches = touches.Where((e) => { return e.phase == TouchPhase.Began; }).ToArray();
+        for (int i = 0; i < touches.Length; i++)
+        {
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touches[i].position);
+            Instantiate(prefab, (Vector3)touchPosition,Quaternion.identity);
+        }
+    }
+}
