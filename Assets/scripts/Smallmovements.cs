a75ab516f1041139de8ac22baa6908fe4ad4d51c
Pepijn Willekens <pepijnwillekens@gmail.com>
2017-02-26 19:26:05 +0100

    added offset to winstate posititons

---
+++ Assets/scripts/Smallmovements.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smallmovements : MonoBehaviour {
+    Vector3 origin;
+    // Use this for initialization
+    public float distance = 0.1f;
+	void Start () {
+        origin = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = (Vector3)Random.insideUnitCircle * distance+ origin;
+
+    }
+}
